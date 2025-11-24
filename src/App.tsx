d2dca97e0ede4d6b4506195851084f1e3c1a71df
Lazlo22 <v.prihodski@gmail.com>
2025-11-24 21:04:50 +0100

    feat: Change background button

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import { Button } from './components/ui/button'
+
 import './App.css'
 
 function App() {
   return (
     <>
-      <div>
-        <h1>Zeely demo</h1>
+      <div className='size-full flex flex-col gap-6 items-center justify-center'>
+        <h1 className='text-4xl font-bold'>Zeely demo</h1>
+        <Button variant='outline' size='lg'>Change Background</Button>
       </div>
     </>
   )
